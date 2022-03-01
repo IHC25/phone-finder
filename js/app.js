e88be4a7fb286e24f935c92e693febf8cdc39501
arefin6 <imranhchy@gmail.com>
2022-03-01 18:42:55 +0600

    show maximum 20 search result added

--- js/app.js
+++ js/app.js
@@ -9,11 +9,12 @@ const loadSearchResult = () => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displaySearchResult(data.data));
+    .then((data) => displaySearchResult(data.data.slice(0, 20)));
 };
 const displaySearchResult = (data) => {
   const phoneContainer = document.getElementById("phone-container");
   phoneContainer.textContent = "";
+  document.getElementById("explore-container").textContent = "";
   // error handling
   errorCheck(data);
   // show data
@@ -26,7 +27,7 @@ const displaySearchResult = (data) => {
           <div class="card-body">
             <h4 class="card-title">${element.phone_name}</h4>
             <h5 class="card-title">${element.brand}</h5>
-            <button onclick="loadExplore('${element.slug}')" class="btn btn-primary btn-lg">Explore</button>
+            <button onclick="loadExplore('${element.slug}')" class="btn btn-primary btn-lg text-bolder">Explore</button>
           </div>
         </div>`;
     phoneContainer.appendChild(div);
@@ -41,51 +42,60 @@ const loadExplore = (id) => {
 };
 
 const displayExploreDetails = (details) => {
-  console.log(details);
   const exploreContainer = document.getElementById("explore-container");
   exploreContainer.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
-  <div class="row g-0 p-2">
-    <div class="col-4 d-flex align-items-center">
-    <img src="${
-      details.image
-    }" class="img-fluid rounded-start h-75" alt="device image" />
-    </div>
-    <div class="col-8">
-      <div class="card-body">
-        <h4 class="card-title">${details.name}</h4>
-        <h5 class="card-title">Features:</h5>
-        <p class="card-text">${details.mainFeatures.storage}</p>
-        <p class="card-text">${details.mainFeatures.displaySize}</p>
-        <p class="card-text">${details.mainFeatures.chipSet}</p>
-        <p class="card-text">${details.mainFeatures.memory}</p>
-        <h5 class="card-title">Sensors:</h5>
-        <p class="card-text">${details.mainFeatures.sensors}</p>
-        <h5 class="card-title">Others:</h5>
-        <p class="card-text">
-          Bluetooth: ${
-            details.others?.Bluetooth
-              ? details.others.Bluetooth
-              : "No Data Found"
-          }, 
-          GPS: ${details.others?.GPS ? details.others.GPS : "No Data Found"}, 
-          NFC: ${details.others?.NFC ? details.others.NFC : "No Data Found"}, 
-          Radio: ${
-            details.others?.Radio ? details.others.Radio : "No Data Found"
-          }, 
-          USB: ${details.others?.USB ? details.others.USB : "No Data Found"}, 
-          WLAN: ${details.others?.WLAN ? details.others.WLAN : "No Data Found"}
-        </p>
-        
-        <p class="card-text">
-          <small class="text-muted">${
-            details.releaseDate ? details.releaseDate : "No release date found"
-          }</small>
-        </p>
+      <div class="row g-0 p-2">
+        <div class="col-4 d-flex align-items-center px-2">
+        <img src="${
+          details.image
+        }" class="img-fluid rounded-start h-75" alt="device image" />
+        </div>
+        <div class="col-8">
+          <div class="card-body">
+            <h4 class="card-title">${details.name}</h4>
+            <h5 class="card-title">Features:</h5>
+            <p class="card-text">${details.mainFeatures.storage}</p>
+            <p class="card-text">${details.mainFeatures.displaySize}</p>
+            <p class="card-text">${details.mainFeatures.chipSet}</p>
+            <p class="card-text">${details.mainFeatures.memory}</p>
+            <h5 class="card-title">Sensors:</h5>
+            <p class="card-text">${details.mainFeatures.sensors}</p>
+            <h5 class="card-title">Others:</h5>
+            <p class="card-text">
+              Bluetooth: ${
+                details.others?.Bluetooth
+                  ? details.others.Bluetooth
+                  : "No Data Found"
+              }, 
+              GPS: ${
+                details.others?.GPS ? details.others.GPS : "No Data Found"
+              }, 
+              NFC: ${
+                details.others?.NFC ? details.others.NFC : "No Data Found"
+              }, 
+              Radio: ${
+                details.others?.Radio ? details.others.Radio : "No Data Found"
+              }, 
+              USB: ${
+                details.others?.USB ? details.others.USB : "No Data Found"
+              }, 
+              WLAN: ${
+                details.others?.WLAN ? details.others.WLAN : "No Data Found"
+              }
+            </p>
+            
+            <p class="card-text">
+              <small class="text-muted">${
+                details.releaseDate
+                  ? details.releaseDate
+                  : "No release date found"
+              }</small>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
   `;
   exploreContainer.appendChild(div);
 };
